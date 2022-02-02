f1799a06597ff4efbbf5a5e8703acbfb6c99735f
xuuuude <x7SF1fxude@gmail.com>
2022-02-03 04:00:12 +0800

    add setting.json and format acwing_717

--- Chapter3/Question/acwing_717_simple_Fibonacci_play_with_ds.cpp
+++ Chapter3/Question/acwing_717_simple_Fibonacci_play_with_ds.cpp
@@ -1,14 +1,17 @@
 #include <iostream>
 using namespace std;
-int main(){
+int main()
+{
 	int i;
 	int a[40];
-	a[0]=0;
-	a[1]=1;
-	cout<<a[0]<<endl<<a[1]<<endl;
-	for(i = 2;i<40;i++){
-		a[i]=a[i-1]+a[i-2];
-		cout<<a[i]<<endl;
+	a[0] = 0;
+	a[1] = 1;
+	cout << a[0] << endl
+		 << a[1] << endl;
+	for (i = 2; i < 40; i++)
+	{
+		a[i] = a[i - 1] + a[i - 2];
+		cout << a[i] << endl;
 	}
 
 	return 0;
