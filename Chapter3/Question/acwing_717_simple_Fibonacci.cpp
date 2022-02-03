a143940aaa7a699c63f244cedb025ca4ef610b61
xuuuude <x7SF1fxude@gmail.com>
2022-02-03 15:40:27 +0800

    del the wrong code about fibonacci

--- Chapter3/Question/acwing_717_simple_Fibonacci.cpp
+++ Chapter3/Question/acwing_717_simple_Fibonacci.cpp
@@ -1,27 +1,18 @@
 #include <iostream>
 using namespace std;
-
-
-
-int fib(n){
-	fib(0) = 0;
-	fib(1) = 1;
-	fib(2) = 1;	
-	fib(n)=fib(n-1)+fib(n-2);
-	return 0;
-}
-
-int main(){
-	int n;
-	cin>>n;
-	while (n--){
-		fib(n);
-		for(int i =4;i<=n;i++){
-		cout<<fib(i)<<endl;
+int main()
+{
+	int i;
+	int a[40];
+	a[0] = 0;
+	a[1] = 1;
+	cout << a[0] << endl
+		 << a[1] << endl;
+	for (i = 2; i < 40; i++)
+	{
+		a[i] = a[i - 1] + a[i - 2];
+		cout << a[i] << endl;
 	}
-}
-	
-//	fib(n);
 
 	return 0;
 }
