b304fe222313a0557d26eb6605882f76dbd1e89b
xuuuude <x7SF1fxude@gmail.com>
2022-02-04 02:29:12 +0800

    add test_grammar_file

---
+++ test_grammar/test_while.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a, b;
+    cin >> a >> b;
+    while (a > b, 0) //中间的逗号有且的意思，括号中表达式的值为：1&0 == 0
+    {
+        cout << "a>b"
+             << " ";
+    }
+
+    return 0;
+}
