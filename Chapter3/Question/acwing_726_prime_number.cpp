fcd63cca25da1a10aad06e8ca13097230330c31d
xuuuude <x7SF1fxude@gmail.com>
2022-02-28 21:57:29 +0800

    review acwing_726

--- Chapter3/Question/acwing_726_prime_number.cpp
+++ Chapter3/Question/acwing_726_prime_number.cpp
@@ -8,13 +8,14 @@ int main()
     {
         int x;
         cin >> x;
-        int i = 2;//除数从2开始
-        for (; i * i <= x; i++)//带等号，x可能会等于i平方，例如x=36，i=6的时候
+        int i = 2;             //除数从2开始
+        for (; i * i < x; i++) //带等号，x可能会等于i平方，例如x=36，i=6的时候
+                               //不带等号在 acwing 上也能 AC
         {
             if (x % i == 0)
                 break;
-                //当x取余i为0时，说明此时输入的x不是素数，break掉当前循环
-        }       //进入下面的if-else 进行判断
+            //当x取余i为0时，说明此时输入的x不是素数，break掉当前循环
+        } //进入下面的if-else 进行判断
         if (i * i > x)
             printf("%d is prime\n", x);
         else
