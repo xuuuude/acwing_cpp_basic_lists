5d25e1fd21c4ea060c882fdf8404043041de2f97
xuuuude <x7SF1fxude@gmail.com>
2022-03-01 00:00:16 +0800

    review code

--- Chapter3/Question/acwing_727_diamond.cpp
+++ Chapter3/Question/acwing_727_diamond.cpp
@@ -5,11 +5,11 @@ int main()
 {
     int n;
     cin >> n;
-    int cx = n / 2;
+    int cx = n / 2;//计算中点
     int cy = n / 2;
     for (int i = 0; i < n; i++)
     {
-        for (int j = 0; j < n; j++)
+        for (int j = 0; j < n; j++)//枚举每一个点
         {
             if (abs(i - cx) + abs(j - cy) <= n / 2)
                 cout << "*";
