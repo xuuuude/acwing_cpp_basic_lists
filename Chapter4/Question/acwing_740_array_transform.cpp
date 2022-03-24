ab8970750eaa4b275b652668dee86223e6b13dad
xuuuude <x7SF1fxude@gmail.com>
2022-03-24 23:50:52 +0800

    brute_force_sol_Q740

--- Chapter4/Question/acwing_740_array_transform.cpp
+++ Chapter4/Question/acwing_740_array_transform.cpp
@@ -1,16 +1,25 @@
 #include <iostream>
+#include <stdio.h>
 using namespace std;
-
-int transform(int a[], int start, int end)
-{
-
-    return 0;
-}
-
+//顺序表的翻转，双指针
 int main()
 {
-    int n[20];
-    int start = 0;
-    int end = sizeof(n) / sizeof(n[0]);
+    int a[20], b[20];
+    for (int i = 0; i < 20; i++)
+    {
+        cin >> a[i];
+    } //给数组 a[i] 赋值
+
+    //倒序取出每个值，正着存到b中
+    for (int i = 19, j = 0; i >= 0, j < 20; i--, j++)
+    {
+        b[j] = a[i]; //存到b中
+    }
+
+    //输出b
+    for (int i = 0; i < 20; i++)
+    {
+        printf("N[%d] = %d\n", i, b[i]);
+    }
     return 0;
 }
